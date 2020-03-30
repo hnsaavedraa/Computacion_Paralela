826c53f5f88fed7a7f5a689b4c32ae79fcb084db
hnsaavedraa <hnsaavedraa@unal.edu.co>
2020-03-29 19:20:24 -0500

    parallel implementation works :')

--- practica1/alg3.c
+++ practica1/alg3.c
@@ -11,6 +11,23 @@
 #include "stb_image/stb_image_write.h"
 
 
+void imprimir(int* scl,int id){
+	printf("Inicio impresion Hilo %i \n",id);
+	for (int z = 0; z < 64; z++)
+     {
+     	if(fmod(z,8) == 0){
+     		printf("\n");
+     	}
+     		
+     		printf("%i ", *(scl+z) );
+  
+
+     }
+     printf("Final impresion Hilo %i \n \n",id);
+     
+}
+
+
 struct parameters
 {
 	int* scl;
@@ -20,28 +37,12 @@ struct parameters
 	int r;
 	int init_i;
 	int fin_i;
+	int id;
 
 };
 
-void parallelProcessH(void* data){
-	struct parameters* dataAux;
-	dataAux = (struct parameters*) data; 
-	printf(" scl en parallel proccessH %i \n", dataAux->scl);
 
 
-	boxBlurH_3(((struct parameters*) data)->tcl,((struct parameters*) data)->scl,dataAux->w,dataAux->h,dataAux->r,dataAux->init_i,dataAux->fin_i);
-
-}
-
-void parallelProcessT(void* data){
-	struct parameters* dataAux;
-	dataAux = (struct parameters*) data; 
-	printf(" scl en parallel proccessT %i \n", dataAux->scl);
-
-
-    boxBlurT_3(((struct parameters*) data)->scl,((struct parameters*) data)->tcl,dataAux->w,dataAux->h,dataAux->r,dataAux->init_i,dataAux->fin_i);
-
-}
 
 
 int max(int num1, int num2)
@@ -79,7 +80,7 @@ void boxesForGauss(int sigma, int n, double* sizes)  // standard deviation, numb
 }
 
 void boxBlurT_3 (int* scl,int* tcl,int w,int h,int r,int init_i,int fin_i) {
-	printf("direccion en T %i \n",tcl);
+
 	// Reemplazar i, j y h,w por los valores de inicio y fin del intervalo
     for(int i=init_i; i<fin_i; i++)
         for(int j=0; j<w; j++) {
@@ -95,7 +96,7 @@ void boxBlurT_3 (int* scl,int* tcl,int w,int h,int r,int init_i,int fin_i) {
 }
 
 void boxBlurH_3 (int* scl,int* tcl,int w,int h,int r,int init_i,int fin_i) {
-	printf("direccion en H %i \n",scl);
+
 	//printf("Entro a boxBlurH_3 \n");
 	// Reemplazar i, j y h,w por los valores de inicio y fin del intervalo
     for(int i=init_i; i<fin_i; i++)
@@ -111,106 +112,80 @@ void boxBlurH_3 (int* scl,int* tcl,int w,int h,int r,int init_i,int fin_i) {
         }
 } 
 
-void boxBlur_3 (int* scl,int* tcl,int w,int h,int r,int numThreads) {
-	int interval_h = floor((h)/ numThreads);
-	printf("tcl Box Blur %i \n",tcl );
-	//printf("interval h %i \n",interval_h );
+void parallelProcess(void* data){
 
-	int threadId[numThreads], i, *retval;
-	pthread_t thread[numThreads];
-
-	int init_i;
-
-	int fin_i;
-
-
-
-	struct parameters data;
-	data.scl = scl;
-	data.tcl = tcl;
-	data.w = w;
-	data.h = h;
-	data.r = r;
-
-
-
-	for(i = 0; i < numThreads; i++){
-        threadId[i] = i;
-        if( i == numThreads-1 ){
-        	fin_i = h;
-        	init_i = i*interval_h;
-        }
-        else{     	
-        	init_i = i*interval_h;
-        	fin_i = (i+1)*interval_h;
-        }
-
-    	data.init_i = init_i;
-		data.fin_i = fin_i;
-		//printf("init i %i \n", init_i);
-
-
-        pthread_create(&thread[i], NULL, (void *)parallelProcessH, &data);
-    
-    }
-
-    for(i = 0; i < numThreads; i++){
-        pthread_join(thread[i], (void **)&retval);
-        printf("Termino hilo %i \n", i);
-    }
-
-    	for(i = 0; i < numThreads; i++){
-        threadId[i] = i;
-        if( i == numThreads-1 ){
-        	fin_i = h;
-        	init_i = i*interval_h;
-        }
-        else{     	
-        	init_i = i*interval_h;
-        	fin_i = (i+1)*interval_h;
-        }
-
-    	data.init_i = init_i;
-		data.fin_i = fin_i;
-		
-
-
-        pthread_create(&thread[i], NULL, (void *)parallelProcessT, &data);
-    
-    }
-
-    for(i = 0; i < numThreads; i++){
-        pthread_join(thread[i], (void **)&retval);
-            int j =0;
-
-
-        unsigned char new_image[w*h];
-		for(int b = 0; b <w*h; b++ ){
-			new_image[j] =  *(tcl+b);
-
-		 	j++;
-
-		}
-
-		
-		char name[] = "imagen .jpg";
-		name[6] = (char)i;
-		stbi_write_jpg(name, w, h, 1, new_image, 100);
-        printf("Termino hilo %i \n", i);
-    }
-
-
-
-
-
-    
-
+	struct parameters* dataAux;
+	dataAux = (struct parameters*) data; 
+	
+	int init_aux = (dataAux->init_i);
+	int fin_aux = (((struct parameters*) data)->fin_i);
+	printf("init_ i en parallel %i en hilo %i \n", dataAux->init_i,dataAux->id);
+	
+	boxBlurH_3(((struct parameters*) data)->tcl,((struct parameters*) data)->scl,dataAux->w,dataAux->h,dataAux->r,init_aux,fin_aux);
+	boxBlurT_3(((struct parameters*) data)->scl,((struct parameters*) data)->tcl,dataAux->w,dataAux->h,dataAux->r,init_aux,fin_aux);
 
 }
 
 
+void boxBlur_3 (int* scl,int* tcl,int w,int h,int r,int numThreads) {
+	int interval_h = floor((h)/ numThreads);
+	int threadId[numThreads], i, *retval;
+	pthread_t thread[numThreads];
+	int init_i;
+	int fin_i;
+
+	struct parameters data_array[numThreads];
+
+	for(i = 0; i < numThreads; i++){
+		struct parameters data;
+		data.scl = scl;
+		data.tcl = tcl;
+		data.w = w;
+		data.h = h;
+		data.r = r;
+    	threadId[i] = i;
+        	
+        if( i == numThreads-1 ){
+        	fin_i = h;
+        	init_i = i*interval_h;
+        	data.init_i = init_i;
+        	data.fin_i = fin_i;
+        }
+        else{     	
+        	init_i = i*interval_h;
+        	fin_i = (i+1)*interval_h;
+        	data.init_i = init_i;
+        	data.fin_i = fin_i;
+        }
+
+		data.id = i;
+		data_array[i] = data;
+		//printf("init i %i \n", init_i);
+		printf("init_ i en create  %i en Hilo %i \n", (data.init_i),i);
+		printf("Direccion de memoria %i en el hilo %i \n",&data_array[i],i );
+
+        pthread_create(&thread[i], NULL, (void *)parallelProcess, &data_array[i]);
+
+    
+    }
+
+    for(i = 0; i < numThreads; i++){
+        //pthread_join(thread[i], (void **)&retval);
+        int retor = pthread_join(thread[i],NULL);
+        printf("scl del hilo %i \n",i );
+        imprimir(scl,i);
+        printf("tcl del hilo %i \n",i );
+        imprimir(tcl,i);
+        printf("RETORNO %i \n",retor );
+        printf("Termino hilo %i \n", i);
+    }
+
+    	
+}
+
+
 void  gaussBlur_3 (int* scl,int* tcl,int w,int h,int r, int numThreads) {
-	printf("tcl gauss Blur %i \n",tcl );
+
 
 	
 	double  *bxs = (double*)malloc(sizeof(double)*3);
@@ -221,8 +196,6 @@ void  gaussBlur_3 (int* scl,int* tcl,int w,int h,int r, int numThreads) {
 		*(tcl+i) = aux;
     }
     boxBlur_3 (scl, tcl, w, h, (int)((*(bxs)-1)/2),numThreads);
-
-    /*
    
     for(int i=0; i<(w*h); i++) {
 		int aux =*(scl+i);
@@ -234,7 +207,7 @@ void  gaussBlur_3 (int* scl,int* tcl,int w,int h,int r, int numThreads) {
 		*(tcl+i) = aux;
     }
     boxBlur_3 (scl, tcl, w, h, (int)((*(bxs+2)-1)/2),numThreads);
-*/
+
 }
 
 
@@ -243,7 +216,7 @@ void  gaussBlur_3 (int* scl,int* tcl,int w,int h,int r, int numThreads) {
 int main(void) {
 
 	int width, height, channels;
-	unsigned char *img = stbi_load("mini.jpg", &width, &height, &channels, 0); //// cero para cargar todos los canales
+	unsigned char *img = stbi_load("1080.jpg", &width, &height, &channels, 0); //// cero para cargar todos los canales
 	if(img == NULL) {
 	 printf("Error in loading the image\n");
 	 exit(1);
@@ -265,14 +238,14 @@ int main(void) {
 		*(b+i) = (uint8_t) *(p + 2);
 
 	}
-
+	imprimir(r,1000);
 
 
 	 int  *r_target = (int*)malloc(sizeof(int)*(img_size/3));
 	 int  *g_target = (int*)malloc(sizeof(int)*(img_size/3));
 	 int  *b_target = (int*)malloc(sizeof(int)*(img_size/3));
 	 int kernel = 15;
-	 int numThreads = 4;
+	 int numThreads = 8;
 	 int j = 0;
 		printf("Direccion r %i \n",r );
 	 //char[] = *r
